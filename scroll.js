894664003b23bf90b06017f1fb54485c86dec497
tobyleungsh <tobyleungszehang@gmail.com>
2024-01-16 10:13:54 +0000

    gg

--- scroll.js
+++
@@ -1,6 +0,0 @@
-import LocomotiveScroll from "/node_modules/locomotive-scroll/dist/locomotive-scroll.js";
-
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true
-});
